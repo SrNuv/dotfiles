ebe05fd613ea32d63d451fead020e19079f311e2
SrNuv <valentin.3ro@gmail.com>
2026-05-11 23:40:36 -0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/i3 ~/.config/i3
+ln -s ~/dotfiles/polybar ~/.config/polybar
+ln -s ~/dotfiles/picom ~/.config/picom
+ln -s ~/dotfiles/nvim ~/.config/nvim
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
